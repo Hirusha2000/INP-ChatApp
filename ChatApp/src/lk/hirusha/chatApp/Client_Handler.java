fa86bcbfab0b0d6f63119debf5968f079e3e45e9
Hirusha2000 <2000hirushaudara@gmail.com>
2023-06-03 12:29:35 +0530

    server

---
+++ ChatApp/src/lk/hirusha/chatApp/Client_Handler.java
@@ -0,0 +1,80 @@
+package lk.hirusha.chatApp;
+
+import java.io.DataInputStream;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.net.Socket;
+import java.util.ArrayList;
+
+public class Client_Handler implements Runnable {
+
+    public static ArrayList<Client_Handler> clientHandlers = new ArrayList<>();
+    private Socket socket;
+    private DataInputStream dataInputStream;
+    private DataOutputStream dataOutputStream;
+    private String clientUserName;
+
+    public Client_Handler(Socket socket) {
+        try {
+            this.socket = socket;
+            this.dataOutputStream = new DataOutputStream(socket.getOutputStream());
+            this.dataInputStream = new DataInputStream(socket.getInputStream());
+
+            this.clientUserName = dataInputStream.readUTF();
+            clientHandlers.add(this);
+            broadcastMessage("SERVER: " + clientUserName + " has entered the chat!");
+        } catch (IOException e) {
+            closeEverything(socket, dataOutputStream, dataInputStream);
+        }
+    }
+
+    @Override
+    public void run() {
+        String messageFromClient;
+
+        while (socket.isConnected()) {
+            try {
+                messageFromClient = dataInputStream.readUTF();
+                broadcastMessage(messageFromClient);
+            } catch (IOException e) {
+                closeEverything(socket, dataOutputStream, dataInputStream);
+                break;
+            }
+        }
+    }
+
+    public void broadcastMessage(String messageToSend){
+        for (Client_Handler clientHandler : clientHandlers) {
+            try {
+                if (!clientHandler.clientUserName.equals(clientUserName)) {
+                    clientHandler.dataOutputStream.writeUTF(messageToSend);
+                    clientHandler.dataOutputStream.flush();
+                }
+            } catch (IOException e) {
+                closeEverything(socket, dataOutputStream, dataInputStream);
+            }
+        }
+    }
+
+    public void removeClientHandler() {
+        clientHandlers.remove(this);
+        broadcastMessage("SERVER: " + clientUserName + " has left the chat!");
+    }
+
+    public void closeEverything(Socket socket, DataOutputStream dataOutputStream, DataInputStream dataInputStream) {
+        removeClientHandler();
+        try {
+            if (dataOutputStream != null) {
+                dataOutputStream.close();
+            }
+            if (dataInputStream != null) {
+                dataInputStream.close();
+            }
+            if (socket != null) {
+                socket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
---
+++ ChatApp/src/lk/hirusha/chatApp/Server.java
@@ -0,0 +1,44 @@
+package lk.hirusha.chatApp;
+
+import java.io.IOException;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class Server {
+    private final ServerSocket serverSocket;
+
+    public Server(ServerSocket serverSocket) {
+        this.serverSocket = serverSocket;
+    }
+
+    public static void main(String[] args) throws IOException {
+        ServerSocket serverSocket = new ServerSocket(6000);
+        Server server = new Server(serverSocket);
+        server.startServer();
+    }
+
+    public void startServer() {
+        try {
+            while (!serverSocket.isClosed()) {
+                Socket socket = serverSocket.accept();
+                System.out.println("A new Client has Connected..");
+                Client_Handler clientHandler = new Client_Handler(socket);
+
+                Thread thread = new Thread(clientHandler);
+                thread.start();
+            }
+        } catch (IOException e) {
+
+        }
+    }
+
+    public void closeServerSocket() {
+        try {
+            if (serverSocket != null) {
+                serverSocket.close();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
