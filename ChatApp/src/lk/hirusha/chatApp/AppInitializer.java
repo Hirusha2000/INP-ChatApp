fe0ef26548bb63425b00f379fc57d4d0c932efa6
Hirusha2000 <2000hirushaudara@gmail.com>
2023-06-03 12:14:28 +0530

    controller class

--- ChatApp/src/lk/hirusha/chatApp/AppInitializer.java
+++ ChatApp/src/lk/hirusha/chatApp/AppInitializer.java
@@ -18,7 +18,7 @@ public class AppInitializer  extends Application {
     }
     @Override
     public  void start (Stage primaryStage) throws IOException {
-        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource())));
+        primaryStage.setScene(new Scene(FXMLLoader.load(getClass().getResource("lk/hirusha/chatApp/view/LoginForm.fxml"))));
         primaryStage.show();
     }
 }
---
+++ ChatApp/src/lk/hirusha/chatApp/controller/LoginFormController.java
@@ -0,0 +1,27 @@
+package lk.hirusha.chatApp.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class LoginFormController {
+    public TextField txtUserName;
+    public AnchorPane loginFormContext;
+
+    public void btnLoginOnAction(ActionEvent actionEvent) {
+        ChatWindowController.userName = txtUserName.getText();
+        try {
+            Stage stage = new Stage();
+            stage.setScene(new Scene(FXMLLoader.load(getClass().getResource("../views/Chat_Window.fxml"))));
+            stage.setTitle("Chat Room");
+            stage.show();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
